82b6464d70945d5c656012f43f1ec4ee6652a291
i32raloa <adolforlopez05@gmail.com>
2026-04-10 16:59:04 +0100

    Add Genetic Algorithm options to main menu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.randomSearch import random_search
 from src.utils import plot_results
 from src.gridSearch import grid_search
+from src.geneticAlgorithm import genetic_algorithm
 
 if __name__ == "__main__":
     while True:
@@ -8,7 +9,9 @@ if __name__ == "__main__":
         print("1. Ejecutar Random Search (600 iteraciones por defecto)")
         print("2. Ejecutar Random Search (Especificar iteraciones)")
         print("3. Grid Search (Rejilla predefinida)")
-        print("4. Salir")
+        print("4. Algoritmo Genético (Generacional)")
+        print("5. Algoritmo Genético (Estacionario)")
+        print("6. Salir")
         
         opcion = input("\nSeleccione una opción: ")
 
@@ -27,6 +30,14 @@ if __name__ == "__main__":
             print(f"\nResultado Grid Search: {best_acc:.4f} en {duration:.2f}s")
             plot_results(history= results_history, n_iter=len(results_history), alg="Grid Search")
         elif opcion == "4":
+            best_p, best_acc, history, duration = genetic_algorithm(mode="generational")
+            print(f"\nResultado AG Generacional: {best_acc:.4f} en {duration:.2f}s")
+            plot_results(history, len(history), "AG Generacional")
+        elif opcion == "5":
+            best_p, best_acc, history, duration = genetic_algorithm(mode="steady-state")
+            print(f"\nResultado AG Estacionario: {best_acc:.4f} en {duration:.2f}s")
+            plot_results(history, len(history), "AG Estacionario")
+        elif opcion == "6":
             print("\nSaliendo...")
             break
         else:
